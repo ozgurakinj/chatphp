a53f708abeb91b7d2b9f81d48eac7fa6582ebb9d
Özgür Akın <ozgurakn14@gmail.com>
2023-02-22 00:20:35 +0300

    add routes to index

--- public/index.php
+++ public/index.php
@@ -5,19 +5,20 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 require __DIR__ . '/../vendor/autoload.php';
+require '../src/db/db.php';
 
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function (Request $request, Response $response, array $args) {
+$app->get('/', function (Request $request, Response $response) {
     $response->getBody()->write("Server running");
     return $response;
 });
 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
+
+#Users
+require "../src/routes/users.php";
+#Chats
+require "../src/routes/chats.php";
 
 $app->run();
