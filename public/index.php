ba1a96eefe42643a76a6c6ede94ee79c7f7beaaf
Özgür Akın <ozgurakn14@gmail.com>
2023-02-21 17:29:02 +0300

    add index

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("Server running");
+    return $response;
+});
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+$app->run();
